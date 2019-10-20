bdf5ca6b5c2085b3b41e6f1ff613b9948debefab
latoyaallen <me@tailorlabs.io>
2019-10-20 12:32:59 -0400

    Handle submit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import api from './Utils/api';
+import Card from './components/Card';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: '',
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-export default App;
+  handleSubmit(e) {
+    e.preventDefault();
+    const url = 'http://localhost:3000/logs'
+    const response = api.loadData(url);
+    console.log("response is...");
+    console.log(response);
+  }
+
+  render() {
+    return (
+      <div>
+        <Card handleSubmit={this.handleSubmit} />
+      </div>
+    );
+  }
+};
+
+  export default App;
