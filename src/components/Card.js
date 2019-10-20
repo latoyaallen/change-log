a965ed2ac30581e28e9ccbd8608d99d3576379b2
latoyaallen <me@tailorlabs.io>
2019-10-20 13:08:37 -0400

    Create two column Card layout

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import Content from './Content'
+
+const row = {
+  display: 'flex',
+}
+
+const columnLeft = {
+  width: '25%'
+}
+
+const columnRight = {
+  width: '75%'
+}
 
 const Card = (props) => (
-  <div>
+  <div style={row}>
+    <div style={columnLeft}>
+      Date
+    </div>
+    <div style={columnRight}>
+      <Content />
+    </div>
     <p onClick={props.handleSubmit}>Click Me</p>
   </div>
 );
 
 export default Card;
+
+// We want to create a two column layout using flex.
+// It's worth noting that IE 10 doesn't support flex.
---
+++ src/components/Content.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Content = (props) => (
+  <div>
+    <div>Title</div>
+    <div>Content</div>
+  </div>
+);
+
+export default Content;
